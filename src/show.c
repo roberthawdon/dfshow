fd2fa94820de6ef8138c7037221895b2d2639918
Robert Hawdon <git@robertianhawdon.me.uk>
2018-03-04 22:09:23 +0000

    Added basic colour, redefined some menus

--- src/show.c
+++ src/show.c
@@ -12,10 +12,13 @@ int exittoshell();
 void directory_view()
 {
   clear();
+  attron(COLOR_PAIR(1));
   mvprintw(0, 0, "Copy, Move, Delete, Edit, Help, Quit, Rename, Show"); // Placehoder for top bar
+  attron(COLOR_PAIR(2));
   mvprintw(2, 2, "/home/ian/projects/dfshow"); // Placeholder for PWD
   mvprintw(3, 2, "14 Objects   42563 Used 10930239 Available"); // Placeholder for PWD info
   mvprintw(4, 4, "-Attrs-     -Owner & Group-  -Size- --Date & Time-- ----Name----"); // Header
+  attron(COLOR_PAIR(1));
   mvprintw(LINES-1, 0, "F1-Down F2-Up F3-Top F4-Bottom F5-Refresh F6-Mark/Unmark F7-All F8-None F9-Sort"); // Placeholder for bottom bar
   refresh();
 
@@ -50,9 +53,12 @@ void quit_menu()
           exittoshell();
           refresh();
           break;
-        case 27:
-          directory_view();
+        case 's':
+          directory_view(); // TODO: Ask which directory to show, this is a temporary placeholder
           break;
+          /* case 27: // Pressing escape here didn't actually do anything in DF-EDIT 2.3b
+          directory_view();
+          break; */
         }
     }
 }
@@ -69,6 +75,9 @@ int main()
 {
 
   initscr();
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_GREEN, COLOR_BLACK);
   cbreak();
   // nodelay(stdscr, TRUE);
   noecho();
