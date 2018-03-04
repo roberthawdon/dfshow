81b3a274a83d02af27e831a961cc9f72af54c005
Robert Hawdon <git@robertianhawdon.me.uk>
2018-03-04 19:51:21 +0000

    Basic key input (q)

--- src/show.c
+++ src/show.c
@@ -1,11 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
-#include <unistd.h>
 
-int main(int argc, char *argv[]) {
+int main() {
+
+  int c;
 
   initscr();
+  cbreak();
+  // nodelay(stdscr, TRUE);
   noecho();
-  curs_set(FALSE);
+  curs_set(FALSE); // Hide Curser (Will want to bring it back later)
+  keypad(stdscr, TRUE);
 
   mvprintw(0, 0, "Copy, Move, Delete, Edit, Help, Quit, Rename, Show"); // Placehoder for top bar
   mvprintw(2, 2, "/home/ian/projects/dfshow"); // Placeholder for PWD
@@ -14,7 +20,21 @@ int main(int argc, char *argv[]) {
   mvprintw(LINES-1, 0, "F1-Down F2-Up F3-Top F4-Bottom F5-Refresh F6-Mark/Unmark F7-All F8-None F9-Sort"); // Placeholder for bottom bar
   refresh();
 
-  sleep(10);
+
+  while(1)
+    {
+      c = getch();
+      switch(c)
+        {
+        case 'q':
+          clear();
+          mvprintw(0, 0, "Change dir, Command, Edit file, Help, Make dir, Quit, Show dir"); // Placehoder for top bar
+          refresh();
+          /* default:
+          mvprintw(LINES-2, 1, "Character pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+          refresh(); */
+        }
+    }
 
   endwin();
 }
