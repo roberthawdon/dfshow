7d9c8bff2b9d7a006944bdd7946215056e3780d6
Robert Hawdon <git@robertianhawdon.me.uk>
2018-05-26 13:44:31 +0100

    Getting user id from string

--- src/functions.c
+++ src/functions.c
@@ -364,12 +364,40 @@ void UpdateOwnerGroup(const char* object, char* ogstr)
 {
   char owner[256];
   char group[256];
-  uid_t uid;
-  gid_t gid;
+  char *buf;
+  size_t bufsize;
+  struct passwd pwd;
+  struct passwd *result;
+  int s;
+  int uid;
+
+  bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
+  if (bufsize == -1)          /* Value was indeterminate */
+    bufsize = 16384;        /* Should be more than enough */
+
+  buf = malloc(bufsize);
+  if (buf == NULL) {
+    perror("malloc");
+    exit(EXIT_FAILURE);
+  }
+
   if ( check_last_char(ogstr, ":") ){
-    ogstr[strlen(ogstr)-1] = 0;
+    ogstr[strlen(ogstr)-1] = 0; // Strip the : from the string
     strcpy(owner, ogstr);
     strcpy(group, ogstr);
+    s = getpwnam_r(owner, &pwd, buf, bufsize, &result);
+    free(buf);
+    if (result == NULL){
+      if (s == 0){
+        move(0,0);
+        clrtoeol();
+        mvprintw(0,0,"Invalid user: %s", owner);
+      }
+    } else {
+      uid = result->pw_uid;
+      mvprintw(0,66, "%d:%d", uid, 91); //test
+    }
+    //gid = getgrnam(group)->gr_gid;
     //mvprintw(0,66, "%s:%s", owner, group); //test
   }
 }
