1b0c48623d3cad6c48bb640dd011e63c4cdbae95
Robert Hawdon <git@robertianhawdon.me.uk>
2019-07-15 20:51:28 +0100

    Cut branch for 0.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Robert Ian Hawdon'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.8.2-beta'
+release = '0.9.0-beta'
 
 
 # -- General configuration ---------------------------------------------------
