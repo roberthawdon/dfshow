570ca51b186487c6d55c3dc510ee1793ad0f6838
Robert Hawdon <robert.hawdon@bedegaming.com>
2019-08-02 18:20:18 +0100

    Fixed sorting issue for large files (#109)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Robert Ian Hawdon'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.8.0-beta'
+release = '0.8.1-beta'
 
 
 # -- General configuration ---------------------------------------------------
