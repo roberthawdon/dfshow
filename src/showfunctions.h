87515bcd7a6c941e32df078e32a37ae706e3e0a3
Robert Hawdon <git@robertianhawdon.me.uk>
2019-11-28 17:27:28 +0000

    Defined column names

--- src/showfunctions.h
+++ src/showfunctions.h
@@ -12,6 +12,14 @@
 #define LEFT  0
 #define RIGHT 1
 
+#define COL_MARK  0
+#define COL_ATTR  1
+#define COL_HLINK 2
+#define COL_OWNER 3
+#define COL_SIZE  4
+#define COL_DATE  5
+#define COL_NAME  6
+
 typedef struct {
   int marked[1];
   mode_t mode;
